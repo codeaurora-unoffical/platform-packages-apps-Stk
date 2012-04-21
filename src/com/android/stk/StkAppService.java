8f01631e617775ae213bd4b23de36eb68823eb7c
Siddartha Pothapragada <spotha@codeaurora.org>
2012-04-20 18:36:10 -0700

    Stk: Show 'Toast indications' SEND DATA / RECV DATA and CLOSE CHANNEL
    Displaying Toast indication yields better UI experience when there are
    multiple SEND / RCV DATA proactive commands in a single OPEN CHANNEL
    session.
    
    CRs-Fixed:  344570
    
    (cherry picked from commit b849067179b63615c8eb88d82664c86a72835f6f)
    
    Change-Id: Ic8c4cd131b2d5660c7cc61505eb5a2f107bb6463

--- src/com/android/stk/StkAppService.java
+++ src/com/android/stk/StkAppService.java
@@ -800,7 +800,10 @@ public class StkAppService extends Service {
                     break;
                 }
             }
-            launchTransientEventMessage();
+            /*
+             * Display indication in the form of a toast to the user if required.
+             */
+            launchEventMessage();
             break;
         }
 
